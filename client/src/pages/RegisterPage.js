0640c08229aea1cfb1fbc96388e01ecd04a58f99
Aryan Bakliwal <aryanbakliwal12345@gmail.com>
2023-05-30 23:26:02 +0530

    first commit

---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function RegisterPage() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    async function register(e) {
+        e.preventDefault();
+        const response = await fetch("http://localhost:4000/register", {
+            method: 'POST',
+            body: JSON.stringify({name, email, username, password}),
+            headers: {'Content-Type': 'application/json'}
+        });
+        if(response.status === 200) {
+            alert('Registration successful.');
+        } else {
+            alert('Registration failed!');
+        }
+    }
+
+    return (
+        <>
+        <form className="register" onSubmit={register}>
+        <h1>Register</h1>
+            <input type="text" placeholder="Name" value={name} onChange={e => {setName(e.target.value)}}/>
+            <input type="email" placeholder="Email" value={email} onChange={e => {setEmail(e.target.value)}}/>
+            <input type="text" placeholder="Username" value={username} onChange={e => {setUsername(e.target.value)}}/>
+            <input type="password" placeholder="Password" value={password} onChange={e => {setPassword(e.target.value)}}/>
+            <button>Register</button>
+        </form>
+        </>
+    );
+}
